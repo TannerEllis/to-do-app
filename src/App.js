80c8842efb29bd7c61d58057647937a154928206
MyNameMatters <ellistanner.3@gmail.com>
2022-06-04 10:15:56 -0600

    added items array to contain new items to list using the usestate hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if (!newItem) {
+      alert("Please add an item");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 10000),
+      value: newItem,
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
+  };
+
+  const deleteItem = (id) => {
+    console.log(id);
+    const newList = items.filter((item) => item.id !== id);
+    setItems(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do</h1>
+      <div className="list-container">
+        <div className="add-input">
+          <input
+            type="text"
+            placeholder="Add to list..."
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button onClick={addItem}>Add</button>
+        </div>
+        <div className="list-item-container">
+          <ul>
+            {items.map((item, i) => {
+              return (
+                <li className="item " key={i}>
+                  {item.value}
+                  <button onClick={() => deleteItem(item.id)}>Delete</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
